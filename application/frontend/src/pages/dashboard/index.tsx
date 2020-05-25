b34ab5fdcb2ce1c6c18405dcc873cd25bf6745cf
gebeto <slavik.nychkalo@gmail.com>
2020-05-26 00:54:25 +0300

    Added one day schedule to dashboard

--- application/frontend/src/pages/dashboard/index.tsx
+++ application/frontend/src/pages/dashboard/index.tsx
@@ -1,25 +1,25 @@
 import * as React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
 
+import { ScheduleDay } from '../schedule/ScheduleList/Day';
+import { createDay } from '../schedule/ScheduleList/helpers';
+
+const day = createDay("05/24/2020");
 
 const Dashboard = (props) => {
 	return (
 		<React.Fragment>
-			<Typography variant="h2">
-				Dashboard
+			<Typography variant="h6" gutterBottom>
+				Розклад найближчих занять
 			</Typography>
-			<Typography paragraph>
-				Hello Slavik, it is Dashboard. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-				ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-				facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-				gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-				donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-				adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-				Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-				imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-				arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-				donec massa sapien faucibus et molestie ac.
-			</Typography>
+			<Grid container spacing={2}>
+				<Grid item xs={12} lg={6}>
+					<ScheduleDay schedule={day} />
+				</Grid>
+			</Grid>
 		</React.Fragment>
 	);
 };
